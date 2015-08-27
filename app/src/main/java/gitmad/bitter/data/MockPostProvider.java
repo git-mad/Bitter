f0ee71658fa512d6fd48db88beb35c3e28ada799
Brian Murphy <brainmurphy1@gmail.com>
2015-08-27 18:00:09 -0400

    added MockPostProvider and gitignored the .idea/misc.xml file

---
+++ app/src/main/java/gitmad/bitter/data/MockPostProvider.java
@@ -0,0 +1,31 @@
+package gitmad.bitter.data;
+
+import android.content.Context;
+
+import gitmad.bitter.R;
+
+/**
+ * Created by brian on 8/27/15.
+ */
+public class MockPostProvider implements PostProviderable {
+
+    private Context context;
+
+    public MockPostProvider(Context context) {
+        this.context = context;
+    }
+
+    @Override
+    public String[] getPosts() {
+        return context.getResources().getStringArray(R.array.mock_posts);
+    }
+
+    @Override
+    public String getPost(int id) throws IllegalArgumentException {
+        String[] posts = context.getResources().getStringArray(R.array.mock_posts);
+        if (id < 0 || id >= posts.length) {
+            throw new IllegalArgumentException("could not find post with id " + id);
+        }
+        return posts[id];
+    }
+}
---
+++ app/src/main/java/gitmad/bitter/data/PostProviderable.java
@@ -0,0 +1,20 @@
+package gitmad.bitter.data;
+
+import java.util.List;
+
+/**
+ * Created by brian on 8/27/15.
+ */
+public interface PostProviderable {
+    /**
+     * @return all of the posts available to this PostProvider
+     */
+    public String[] getPosts();
+
+    /**
+     * @param index the id or index of the desired post
+     * @return the post with the index or id specified.
+     * @throws IllegalArgumentException if a post with the id or index specified is not found
+     */
+    public String getPost(int index) throws IllegalArgumentException;
+}
