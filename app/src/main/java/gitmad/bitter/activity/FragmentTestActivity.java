493c6884616e7744af25c9f5a98c98154e89b39c
Tejasvi Nareddy <tejunareddy@gmail.com>
2015-10-10 14:15:12 -0400

    Attempt to create a test Fragment Manager to run my Fragment

---
+++ app/src/main/java/gitmad/bitter/activity/FragmentTestActivity.java
@@ -0,0 +1,16 @@
+package gitmad.bitter.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+import gitmad.bitter.R;
+
+public class FragmentTestActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragment_test);
+    }
+
+}
