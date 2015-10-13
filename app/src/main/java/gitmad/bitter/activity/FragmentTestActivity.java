f1005a0572596288247fe026d407bd4e5988a59b
Tejasvi Nareddy <tejunareddy@gmail.com>
2015-10-12 21:34:27 -0400

    Fixed bug to allow FragmentTestActivity to work

--- app/src/main/java/gitmad/bitter/activity/FragmentTestActivity.java
+++ app/src/main/java/gitmad/bitter/activity/FragmentTestActivity.java
@@ -2,12 +2,12 @@ package gitmad.bitter.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 
 import gitmad.bitter.R;
 import gitmad.bitter.fragment.UserProfileFragment;
 
-public class FragmentTestActivity extends Activity implements UserProfileFragment.OnFragmentInteractionListener {
-
+public class FragmentTestActivity extends FragmentActivity implements UserProfileFragment.OnFragmentInteractionListener {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
