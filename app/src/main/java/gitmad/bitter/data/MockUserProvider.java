cec631984e6e55ae6b618cde363c0bcb9bce8332
Brian Murphy <brianmurphy@gatech.edu>
2015-12-19 23:17:28 -0500

    modified models to be read only

---
+++ app/src/main/java/gitmad/bitter/data/MockUserProvider.java
@@ -0,0 +1,7 @@
+package gitmad.bitter.data;
+
+/**
+ * Created by brian on 12/18/15.
+ */
+public class MockUserProvider {
+}
---
+++ app/src/main/java/gitmad/bitter/model/Comment.java
@@ -0,0 +1,7 @@
+package gitmad.bitter.model;
+
+/**
+ * Created by brian on 12/18/15.
+ */
+public class Comment {
+}
