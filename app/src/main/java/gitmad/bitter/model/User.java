c8f26e36d24ad860be920f42b7b7e44f44a02103
Hrushikesh (Rishi) Yemme <rishi.yemme@gmail.com>
2016-02-29 19:01:35 -0500

    Updated User class with new instance fields and accessor methods. Also created an Arraylist of "enemies" with some appropriate methods.

--- app/src/main/java/gitmad/bitter/model/User.java
+++ app/src/main/java/gitmad/bitter/model/User.java
@@ -1,16 +1,33 @@
 package gitmad.bitter.model;
 
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+import gitmad.bitter.data.UserProvider;
+import gitmad.bitter.data.firebase.FirebaseUserProvider;
+
 /**
  * Models a user
  */
 public class User {
     private String name;
     private String id;
+    private int numPosts;
+    private int numEnemies;
+    private List<String> enemies;
+    private int numDownVotes;
+    private UserProvider database;
 
     public User(String pName, String pId) {
         name = pName;
         id = pId;
+        numPosts = 0;
+        numDownVotes = 0;
+        enemies = new ArrayList<String>();
+        numEnemies = enemies.size();
+        database = new FirebaseUserProvider();
     }
 
     public User() {
@@ -23,6 +40,30 @@ public class User {
     public String getId() {
         return id;
     }
+
+    public int getNumEnemies() {return numEnemies;}
+
+    public boolean addEnemy(String userId) {
+        if (!enemies.contains(userId)) {
+            enemies.add(userId);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public User removeEnemy(String userId) {
+         if (enemies.remove(userId)) {
+             User user = database.getUser(userId);
+             return user;
+         } else {
+             throw new NoSuchElementException("No Such Enemy Exists");
+         }
+    }
+
+    public int getNumPosts() { return numPosts; }
+
+    public int getNumDownVotes() {return numDownVotes;}
 
     @Override
     public int hashCode() {
