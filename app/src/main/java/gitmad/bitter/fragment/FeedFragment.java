cf7d7f45a7030bb799da09fe06a4dcd65cf1409c
Tejasvi Nareddy <tejunareddy@gmail.com>
2016-03-08 00:02:55 -0500

    Small bug fixes.

--- app/src/main/java/gitmad/bitter/fragment/FeedFragment.java
+++ app/src/main/java/gitmad/bitter/fragment/FeedFragment.java
@@ -8,7 +8,6 @@ import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.NotificationCompat;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
