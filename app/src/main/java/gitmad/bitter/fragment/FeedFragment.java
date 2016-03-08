14e57bbc607cfce31a2f53d6d7dc98efcc7e5183
Austin Leal <bobzeka@gmail.com>
2016-03-07 19:14:47 -0500

    Added take picture functionality to floating action button.

--- app/src/main/java/gitmad/bitter/fragment/FeedFragment.java
+++ app/src/main/java/gitmad/bitter/fragment/FeedFragment.java
@@ -2,17 +2,25 @@ package gitmad.bitter.fragment;
 
 
 import android.content.Intent;
-import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.Map;
 
 import gitmad.bitter.R;
@@ -30,6 +38,7 @@ public class FeedFragment extends Fragment implements AuthorPostDialogFragment.O
     private RecyclerView recyclerView;
     private RecyclerView.Adapter adapter;
     private RecyclerView.LayoutManager layoutManager;
+    private String imagePath;
 
     private MockPostProvider postProvider;
 
@@ -49,9 +58,9 @@ public class FeedFragment extends Fragment implements AuthorPostDialogFragment.O
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_feed, container, false);
+        final View view = inflater.inflate(R.layout.fragment_feed, container, false);
 
-        FloatingActionButton createPost = (FloatingActionButton) view.findViewById(R.id.create_post_button);
+        final FloatingActionButton createPost = (FloatingActionButton) view.findViewById(R.id.create_post_button);
         final FloatingActionButton takePic = (FloatingActionButton) view.findViewById(R.id.camera_fab);
         final FloatingActionButton picFromGallery = (FloatingActionButton) view.findViewById(R.id.gallery_fab);
         final FloatingActionButton textPost = (FloatingActionButton) view.findViewById(R.id.text_fab);
@@ -70,7 +79,7 @@ public class FeedFragment extends Fragment implements AuthorPostDialogFragment.O
                     takePic.show();
                     picFromGallery.show();
                     textPost.show();
-
+                    takePicHandler(takePic);
                 }
 
 
@@ -128,5 +137,63 @@ public class FeedFragment extends Fragment implements AuthorPostDialogFragment.O
     private void showCreatePostDialog() {
         AuthorPostDialogFragment authorPostDialogFragment = AuthorPostDialogFragment.newInstance();
         authorPostDialogFragment.show(getActivity().getSupportFragmentManager(), AuthorPostDialogFragment.AUTHOR_POST_DIALOG_FRAG_TAG);
+    }
+
+    /**
+     * Sets listeners for floating action buttons
+     * @param takePic floating action button to set listener to
+     */
+    private void takePicHandler(FloatingActionButton takePic) {
+
+        takePic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);;
+                if (intent.resolveActivity(getContext().getPackageManager()) != null) {
+                    File file;
+                    try {
+                        file = createFile();
+                    } catch (IOException e) {
+                        file = null;
+                    }
+                    if (file != null) {
+                        intent.putExtra(MediaStore.EXTRA_OUTPUT,
+                                Uri.fromFile(file));
+                        final int IMAGE_REQUEST_CODE = 1;
+                        startActivityForResult(intent, IMAGE_REQUEST_CODE);
+                    }
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 1) {
+            final int resultOk = -1;
+            if (resultCode == resultOk) {
+                Toast.makeText(getActivity(), "Image saved.",
+                        Toast.LENGTH_LONG).show();
+                //image should be accessed here using filename imagePath
+            }
+        }
+    }
+
+    /**
+     * Creates file for saving photo
+     * @return File for saving photo
+     * @throws IOException File cannot be created
+     */
+    public File createFile() throws IOException {
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String fileName = "JPEG_" + timeStamp + "_";
+        File storageDir = Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                fileName,
+                ".jpg",
+                storageDir);
+        imagePath = "file:" + image.getAbsolutePath();
+        return image;
     }
 }
