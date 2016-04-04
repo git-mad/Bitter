868772df8f77235873885f8abfa0d667922bf98e
Kartik Prabhu <kprabhu7@gatech.edu>
2016-04-04 19:00:45 -0400

    Made ViewPostFragment get data from Firebase asynchronously

--- app/src/main/java/gitmad/bitter/fragment/ViewPostFragment.java
+++ app/src/main/java/gitmad/bitter/fragment/ViewPostFragment.java
@@ -2,6 +2,7 @@ package gitmad.bitter.fragment;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.TextInputLayout;
 import android.support.v4.app.Fragment;
@@ -16,11 +17,14 @@ import android.widget.TextView;
 import java.util.Map;
 
 import gitmad.bitter.R;
-import gitmad.bitter.data.mock.MockCommentProvider;
-import gitmad.bitter.data.mock.MockPostProvider;
-import gitmad.bitter.data.mock.MockUserProvider;
 import gitmad.bitter.data.PostProvider;
 import gitmad.bitter.data.UserProvider;
+import gitmad.bitter.data.firebase.FirebaseCommentProvider;
+import gitmad.bitter.data.firebase.FirebasePostProvider;
+import gitmad.bitter.data.firebase.FirebaseUserProvider;
+import gitmad.bitter.data.firebase.auth.FirebaseAuthManager;
+import gitmad.bitter.data.mock.MockPostProvider;
+import gitmad.bitter.data.mock.MockUserProvider;
 import gitmad.bitter.model.Comment;
 import gitmad.bitter.model.Post;
 import gitmad.bitter.model.User;
@@ -29,68 +33,10 @@ import gitmad.bitter.ui.CommentAdapter;
 public class ViewPostFragment extends Fragment {
 
     private static final String KEY_POST_ID = "postIdKey";
-    public ImageButton imageButton;
+    private ImageButton imageButton;
     private boolean clicked = false;
-
-    public static ViewPostFragment newInstance(String postId) {
-        Bundle args = new Bundle();
-        args.putString(KEY_POST_ID, postId);
-        ViewPostFragment fragment = new ViewPostFragment();
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     private Post post;
-    /**
-     * Mandatory empty constructor for the fragment manager to instantiate the
-     * fragment (e.g. upon screen orientation changes).
-     */
-    public ViewPostFragment() {
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        if (getArguments() != null && getArguments().containsKey(KEY_POST_ID)) {
-            String postId = getArguments().getString(KEY_POST_ID);
-            post = getPostFromMockProvider(postId);
-        }
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_view_post, container, false);
-
-        imageButton = (ImageButton) view.findViewById(R.id.imageButton);
-        imageButton.setOnClickListener(imgButtonHandler);
-        TextView postBodyTextView = (TextView) view.findViewById(R.id.postContent);
-        TextView userTextView = (TextView) view.findViewById(R.id.posterUsername);
-
-        postBodyTextView.setText(post.getText());
-        userTextView.setText(getUserFromMockProvider().getName());
-
-        // FIXME temp profile pic, get pic from variable instead of hardcoded id
-        ImageView pic = (ImageView) view.findViewById(R.id.posterPic);
-        Bitmap bm = BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher);
-        Bitmap conv_bm = UserProfileFragment.getRoundedRectBitmap(bm, 500);
-        pic.setImageBitmap(conv_bm);
-
-
-        Comment[] comments = new MockCommentProvider(getContext()).getCommentsOnPost(post.getId());
-
-        Map<Comment, User> authorsMap = new MockUserProvider().getAuthorsOfComments(comments);
-
-        CommentAdapter adapter = new CommentAdapter(this.getActivity(), comments, authorsMap);
-        ListView listView = (ListView) view.findViewById(R.id.comments_list_view);
-        listView.setAdapter(adapter);
-
-        final TextInputLayout commentWrapper = (TextInputLayout) view.findViewById(R.id.comment_text_wrapper);
-        commentWrapper.setHint("Bitch about it!");
-
-        return view;
-    }
-
+    private String postId;
     private View.OnClickListener imgButtonHandler = new View.OnClickListener() {
         public void onClick(View v) {
             if (!clicked) {
@@ -103,6 +49,43 @@ public class ViewPostFragment extends Fragment {
             //implement some way to store downvote
         }
     };
+    /**
+     * Mandatory empty constructor for the fragment manager to instantiate the
+     * fragment (e.g. upon screen orientation changes).
+     */
+    public ViewPostFragment() {
+    }
+
+    public static ViewPostFragment newInstance(String postId) {
+        Bundle args = new Bundle();
+        args.putString(KEY_POST_ID, postId);
+        ViewPostFragment fragment = new ViewPostFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (getArguments() != null && getArguments().containsKey(KEY_POST_ID)) {
+            postId = getArguments().getString(KEY_POST_ID);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_view_post, container, false);
+
+        getPostFirebaseTask postFirebaseTask = new getPostFirebaseTask(view);
+        postFirebaseTask.execute(postId);
+
+        FirebaseAuthManager authManager = new FirebaseAuthManager(getActivity());
+        getCommentFirebaseTask asyncTask = new getCommentFirebaseTask(view);
+        asyncTask.execute(postId);
+
+        return view;
+    }
 
     private Post getPostFromMockProvider(String postId) {
         PostProvider postProvider = new MockPostProvider(getActivity());
@@ -113,4 +96,67 @@ public class ViewPostFragment extends Fragment {
         UserProvider mockUserProvider = new MockUserProvider();
         return mockUserProvider.getAuthorOfPost(post);
     }
+
+    private class getPostFirebaseTask extends AsyncTask<String, String, Post> {
+        private View view;
+        private Post post;
+
+        public getPostFirebaseTask(View view) {
+            this.view = view;
+        }
+
+        protected Post doInBackground(String... params) {
+            FirebasePostProvider firebasePostProvider = new FirebasePostProvider();
+            post = firebasePostProvider.getPost(params[0]);
+            return post;
+        }
+
+        protected void onPostExecute(Post post) {
+            imageButton = (ImageButton) view.findViewById(R.id.imageButton);
+            imageButton.setOnClickListener(imgButtonHandler);
+            TextView postBodyTextView = (TextView) view.findViewById(R.id.postContent);
+            TextView userTextView = (TextView) view.findViewById(R.id.posterUsername);
+
+            postBodyTextView.setText(post.getText());
+            userTextView.setText(getUserFromMockProvider().getName());
+
+            // FIXME temp profile pic, get pic from variable instead of hardcoded id
+            ImageView pic = (ImageView) view.findViewById(R.id.posterPic);
+            Bitmap bm = BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher);
+            Bitmap conv_bm = UserProfileFragment.getRoundedRectBitmap(bm, 500);
+            pic.setImageBitmap(conv_bm);
+
+            final TextInputLayout commentWrapper = (TextInputLayout) view.findViewById(R.id.comment_text_wrapper);
+            commentWrapper.setHint("Bitch about it!");
+
+        }
+    }
+
+    private class getCommentFirebaseTask extends AsyncTask<String, String, Comment[]> {
+        private View view;
+        private Comment[] comments;
+        private Map<Comment, User> authorsMap;
+
+        public getCommentFirebaseTask(View view) {
+            this.view = view;
+        }
+
+        protected Comment[] doInBackground(String... params) {
+            FirebaseCommentProvider firebaseCommentProvider = new FirebaseCommentProvider();
+            comments = firebaseCommentProvider.getCommentsOnPost(params[0]);
+
+            FirebaseUserProvider firebaseUserProvider = new FirebaseUserProvider();
+            authorsMap = firebaseUserProvider.getAuthorsOfComments(comments);
+
+            return comments;
+        }
+
+        protected void onPostExecute(Comment[] comments) {
+            CommentAdapter adapter = new CommentAdapter(view.getContext(), comments, authorsMap);
+            ListView listView = (ListView) view.findViewById(R.id.comments_list_view);
+            listView.setAdapter(adapter);
+
+        }
+    }
+
 }
--- app/src/main/java/gitmad/bitter/model/User.java
+++ app/src/main/java/gitmad/bitter/model/User.java
@@ -5,15 +5,34 @@ package gitmad.bitter.model;
  * Models a user
  */
 public class User {
-    private String name;
     private String id;
+    private String imageId;
+    private String name;
+    private int numEnemies;
+    private int posts;
+    private int salt;
+    private int totalComments;
+    private int totalVotes;
+    private String userSince;
 
-    public User(String pName, String pId) {
-        name = pName;
-        id = pId;
+    public User(String name, String imageId, String id, int numEnemies, int posts, int salt, int totalComments, int totalVotes, String userSince) {
+        this.name = name;
+        this.imageId = imageId;
+        this.id = id;
+        this.numEnemies = numEnemies;
+        this.posts = posts;
+        this.salt = salt;
+        this.totalComments = totalComments;
+        this.totalVotes = totalVotes;
+        this.userSince = userSince;
     }
 
     public User() {
+    }
+
+    public User(String temp, String userUid) {
+        this.name = temp;
+        this.id = userUid;
     }
 
     public String getName() {
@@ -23,6 +42,7 @@ public class User {
     public String getId() {
         return id;
     }
+
 
     @Override
     public int hashCode() {
@@ -38,5 +58,33 @@ public class User {
         User other = (User) o;
 
         return other.getId().equals(getId());
+    }
+
+    public String getImageId() {
+        return imageId;
+    }
+
+    public int getNumEnemies() {
+        return numEnemies;
+    }
+
+    public int getPosts() {
+        return posts;
+    }
+
+    public int getSalt() {
+        return salt;
+    }
+
+    public int getTotalComments() {
+        return totalComments;
+    }
+
+    public int getTotalVotes() {
+        return totalVotes;
+    }
+
+    public String getUserSince() {
+        return userSince;
     }
 }
